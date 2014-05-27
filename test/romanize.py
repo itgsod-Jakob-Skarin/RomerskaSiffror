4162ea389ac4f2d9adb486a0306a3ab677fc8edf
itgsod-Jakob-Skarin <jakob.skarin@elev.it-gymnasiet.se>
2014-05-27 11:34:42 +0200

    Början av RomerskaSiffror
    4/8 fel kvar. 4 klarade!!!!!! Nu är det bara det är om själva
    romanizing kvar.

---
+++ test/romanize.py
@@ -0,0 +1,8 @@
+def romanize(tal):
+    Rtal = ''
+    numbers = [1000, 500, 100, 50, 10, 5, 1]
+    if tal == 0:
+        raise ValueError('can not encode zero')
+    if tal < 0:
+        raise ValueError('can not encode negative number')
+    return Rtal
