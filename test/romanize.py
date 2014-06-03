6ab1a9f24279273d2e188dea522a7d412019109b
itgsod-Jakob-Skarin <jakob.skarin@elev.it-gymnasiet.se>
2014-06-03 11:07:27 +0200

    Klar, helt ändrad
    Den är nu skriven som listor i en lista istället för med dictionary.
    Dictionary var för komplicerad för en uppgift där man klarade det
    lättare med listor i listor, som också gav kortare kod(?).

--- test/romanize.py
+++ test/romanize.py
@@ -1,8 +1,19 @@
 def romanize(tal):
-    Rtal = ''
-    numbers = [1000, 500, 100, 50, 10, 5, 1]
+    Romerskt_tal = ''
+    tal_list_list = [['M', 1000], ['CM', 900], ['D', 500], ['CD', 400], ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
+                     ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]]
+
     if tal == 0:
         raise ValueError('can not encode zero')
     if tal < 0:
         raise ValueError('can not encode negative number')
-    return Rtal
+
+
+    for list in tal_list_list:
+        while tal - list[1] >= 0:
+            tal = tal - list[1]
+            Romerskt_tal += list[0]
+
+    return Romerskt_tal
+
+print(romanize(44))
